a23785ca39437688f841da8d6cf13039b1081401
Ivo Shurbanovski <shurbanovski.i@gmail.com>
2024-02-20 22:02:38 +0100

    Tests in PFT Service layer.

---
+++ personal_finance_tracker/service/src/test/java/com/parcels/infrastructure/db/PostgresContainerInitializer.java
@@ -0,0 +1,16 @@
+package com.parcels.infrastructure.db;
+
+import lombok.Getter;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@Testcontainers
+@Getter
+public class PostgresContainerInitializer {
+
+    @Container
+    public static PostgreSQLContainer<PostgresTestContainer> container =
+            PostgresTestContainer.getInstance();
+}
